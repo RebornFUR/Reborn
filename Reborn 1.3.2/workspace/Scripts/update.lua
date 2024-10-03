973f138593518e5ef2ca305d0574397b0bfbeb49
RebornFUR <spamac0709@outlook.com>
2024-10-03 13:37:50 +0100

    Add files via upload

---
+++ Reborn 1.3.2/workspace/Scripts/update.lua
@@ -0,0 +1,9 @@
+[+] added
+[*] changed
+[-] removed
+-- [+] save file
+-- [+] open file
+-- [+] full UI rework
+-- [-] status and roblox open
+-- [+] Kill Roblox (readded)
+-- [+] preloaded scripts (readded)
